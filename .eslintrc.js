d5958f6e9708a2300314cc0bf60d2a92a19fb903
Dominick Hem <domhem@csu.fullerton.edu>
2018-03-19 17:29:53 -0700

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": "off",
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+Dominick Hem
+
+CPSC 473-02
+
+HW 5-Chattrbox with Chapter 15 Bronze and Silver Challenge/Topic commands
